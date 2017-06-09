e42df8a2ce8d0e33f0e4f2af78e17237a1f2f2cc
Giovanni Bonafè <midable@gmail.com>
2017-06-09 18:57:37 +0200

    core set of indicators

--- R/indicators.R
+++ R/indicators.R
@@ -2,12 +2,12 @@
 #'
 #' @param obs numeric vector of observed values
 #' @param mod numeric vector of modelled values
-#'
-#' @return RMSE as in eq.1, tab.1, p.18 in Janssen et al., 2017
 #' @references Janssen et al., 2017. "Guidance Document on Modelling Quality Objectives and Benchmarking. Version 2.1"
-#' @rdname indicators
 #' @title Core Set of Statistical Indicators
 #' @name indicators
+#'
+#' @return \code{RMSE}: Root Mean Square Error as in eq.1, tab.1, p.18 in Janssen et al., 2017
+#' @rdname indicators
 RMSE <- function(obs, mod) {
   dd <- (obs-mod)^2
   sqrt(sum(dd, na.rm=T)/length(dd))
@@ -15,7 +15,7 @@ RMSE <- function(obs, mod) {
 
 #' \code{R}: Correlation coefficient
 #'
-#' @return R as in eq.2, tab.1, p.18 in Janssen et al., 2017
+#' @return \code{R}: Correlation coefficient as in eq.2, tab.1, p.18 in Janssen et al., 2017
 #' @rdname indicators
 R <- function(obs, mod) {
   obs <- obs+mod-mod
@@ -30,7 +30,7 @@ R <- function(obs, mod) {
 
 #' \code{BIAS}: Bias
 #'
-#' @return BIAS as in eq.3, tab.1, p.18 in Janssen et al., 2017
+#' @return \code{BIAS}: bias as in eq.3, tab.1, p.18 in Janssen et al., 2017
 #' @rdname indicators
 BIAS <- function(obs, mod) {
   obs <- obs+mod-mod
@@ -42,7 +42,7 @@ BIAS <- function(obs, mod) {
 
 #' \code{NMB}: Normalised Mean Bias
 #'
-#' @return NMB as in eq.3, tab.1, p.18 in Janssen et al., 2017
+#' @return \code{NMB}: Normalised Mean Bias as in eq.3, tab.1, p.18 in Janssen et al., 2017
 #' @rdname indicators
 NMB <- function(obs, mod) {
   obs <- obs+mod-mod
@@ -54,7 +54,7 @@ NMB <- function(obs, mod) {
 
 #' \code{NMSD}: Normalised Mean Standard Deviation
 #'
-#' @return NMSD as in eq.4, tab.1, p.18 in Janssen et al., 2017
+#' @return \code{NMSD}: Normalised Mean Standard Deviation as in eq.4, tab.1, p.18 in Janssen et al., 2017
 #' @rdname indicators
 NMSD <- function(obs, mod) {
   obs <- obs+mod-mod
